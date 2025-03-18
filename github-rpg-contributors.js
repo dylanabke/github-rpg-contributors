d64c9d51a25a15504e39b208d807f089257cac12
dylanabke <dja5837@psu.edu>
2025-03-18 12:56:24 -0400

    Added my old code as I changed name

--- github-rpg-contributors.js
+++ github-rpg-contributors.js
@@ -5,22 +5,26 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/rpg-character/rpg-character.js';
 
 /**
- * `github-rpg-contributors`
+ * `rpg-character`
  * 
  * @demo index.html
- * @element github-rpg-contributors
+ * @element rpg-character
  */
-export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
+export class RpgCharacter extends DDDSuper(I18NMixin(LitElement)) {
 
   static get tag() {
-    return "github-rpg-contributors";
+    return "rpg-character";
   }
 
   constructor() {
     super();
     this.title = "";
+    this.items = [];
+    this.organization = "haxtheweb";
+    this.repository = "webcomponents";
     this.t = this.t || {};
     this.t = {
       ...this.t,
@@ -29,17 +33,23 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/github-rpg-contributors.ar.json", import.meta.url).href +
+        new URL("./locales/rpg-character.ar.json", import.meta.url).href +
         "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
+
+  
 
   // Lit reactive properties
   static get properties() {
     return {
       ...super.properties,
       title: { type: String },
+      items: { type: Array },
+      organization: { type: String },
+      repository: { type: String },
+      limit: { type: Number },
     };
   }
 
@@ -58,20 +68,67 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
         padding: var(--ddd-spacing-4);
       }
       h3 span {
-        font-size: var(--github-rpg-contributors-label-font-size, var(--ddd-font-size-s));
+        font-size: var(--rpg-character-label-font-size, var(--ddd-font-size-s));
       }
     `];
+  }
+
+  getData() {
+    const url = `https://api.github.com/repos/${this.organization}/${this.repository}/contributors`;
+    try{
+      fetch(url).then(d => d.ok ? d.json(): {}).then(data => {
+        if (data) {
+  
+          this.items = [data];
+          console.log(this.items);
+        }  
+      }
+    }
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <h2>${this.title}</h2>
+    <details open>
+      <summary>Search inputs</summary>
+      <div>
+
+      </div>
+    </details>
+    <div class="results">
+      ${this.items.map((item, index) => html`
+      <rpg-character
+        seed="${item.login}"
+      ></rpg-character>
+      `)}
+    </div>
+    `;
   }
 
+  
+  // life cycle will run when anything defined in `properties` is modified
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    // see if value changes from user input and is not empty
+    //if (changedProperties.has('organization')) {
+     // this.updateResults();
+    //}
+    this.updateResults();
+    
+  }
+
+  updateResults() {
+    fetch(`https://api.github.com/repos/${this.organization}/${this.repository}/contributors`).then(d => d.ok ? d.json(): {}).then(data => {
+      if (data) {
+        this.items = [];
+        this.items = data;
+        console.log(this.items);
+      
+      }  
+    });
+  }
+  
   /**
    * haxProperties integration via file reference
    */
@@ -81,4 +138,4 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
   }
 }
 
-globalThis.customElements.define(GithubRpgContributors.tag, GithubRpgContributors);
+globalThis.customElements.define(RpgCharacter.tag, RpgCharacter);
