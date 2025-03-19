9345694f1317bf457245a10c0861a1c51ee84e95
dylanabke <dja5837@psu.edu>
2025-03-19 13:20:55 -0400

    Updating

--- github-rpg-contributors.js
+++ github-rpg-contributors.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/rpg-character/rpg-character.js';
 
 /**
  * `github-rpg-contributors`
@@ -21,6 +22,9 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
+    this.items = [];
+    this.organization = "haxtheweb";
+    this.repository = "webcomponents";
     this.t = this.t || {};
     this.t = {
       ...this.t,
@@ -40,6 +44,9 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      items: { type: Array },
+      organization: { type: String },
+      repository: { type: String },
     };
   }
 
@@ -66,11 +73,44 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <div class="wrapper">
+      <h3>GitHub Repo: <a href="https://github.com/${this.org}/${this.repo}">${this.org}/${this.repo}</a></h3>
+      <slot></slot>
+      ${this.items.filter((item, index) => index < 5).map((item) => 
+          html`
+          <div class="rpg-wrapper">
+          <rpg-character  seed="${item.login}"></rpg-character>
+          <div class="contdetails">
+          ${item.login}
+          Contributions: ${item.contributions}
+          </div>
+          </div>
+          `)}
+    </div>`;
   }
+
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    // see if value changes from user input and is not empty
+    if (changedProperties.has('organization')) {
+      this.getData();
+    }
+    
+  }
+
+  getData() {
+    console.log("HI");
+    const url = `https://api.github.com/repos/${this.organization}/${this.repository}/contributors`;
+    try {
+      fetch(url).then(d => d.ok ? d.json(): {}).then(data => {
+        if (data) {
+          this.items = [];
+          this.items = data;
+          console.log(this.items);
+        }});
+    } catch (error) {
+      console.error("HI");
+    }}
 
   /**
    * haxProperties integration via file reference
